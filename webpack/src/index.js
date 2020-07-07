97f66908dabe95fb113cb245870344da3558c9c6
afc <905306092@qq.com>
2020-07-07 20:22:08 +0800

    this parts learn use webpack to load asset Loading CSS Loading Fonts Loading Data (csv,tsv,xml) Global Asset: just like vuejs, put all the assets js in one folder

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,9 +1,30 @@
 import _ from 'lodash';
+//引入css文件的语法，而它的实现会调用webpack.config.js中
+//设置的rule
+import './style.css';
+
+//引入图片的语法，把图片转换为一个对象，
+//它的实现会调用webpack.config.js中的rule
+import catHouse from "./cat-house.jpg";
+
+//载入xml文件，调用xml-loader来加载
+import Data from './data.xml';
+
 
 function component() {
     const element = document.createElement('div');
 
+    //Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add("hello");
+
+    //Add the image to our existing div.
+    const myIcon = new Image();
+    myIcon.src = catHouse; //相当于myIcon.src="./cat-house.jpg"
+
+    element.appendChild(myIcon);
+
+    console.log(Data);
     return element;
 }
 document.body.appendChild(component());
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -3,7 +3,44 @@ const path = require("path");
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [{
+            test: /\.css$/, //筛选适用于该rule的文件的pattern
+            use: [
+                //用于处理css文件的loader，调用顺序是由后往前
+                //先调用css-loader, 然后style-loader
+                'style-loader',
+                'css-loader',
+            ],
+
+        }, { //加载图片的loader
+            test: /\.(png|svg|jpg|gif)$/,
+            use: [
+                'file-loader', //只是加载文件，相当于copy，并改为hash命名
+            ],
+
+        }, { //字体的加载
+            test: /\.(woff|woff2|eot|ttf|otf)$/,
+            use: [
+                'file-loader',
+            ]
+        }, {
+            //tsv: tab separated values
+            //csv: comma separated values
+            //纯文本格式的电子表格的加载
+            test: /\.(tsv|csv)$/,
+            use: [
+                'csv-loader',
+            ]
+        }, {
+            //xml可拓展标记语言文件的加载
+            test: /\.xml$/,
+            use: [
+                'xml-loader',
+            ]
+        }],
     },
 }
